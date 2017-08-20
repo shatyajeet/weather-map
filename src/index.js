2e7373a74551e74a4c20d9ed19f79921e2990229
Satyajeet Parida <shatyajeet@gmail.com>
2017-08-20 20:52:03 +0530

    fix(Added src and fixed eslint): Added src file and fixes eslint configuration

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export const weatherMap = () => {
+  console.log('This is the weather map');
+};
---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+import {expect} from 'chai';
+
+describe('weatherMap', () => {
+  it('should be working', () => {
+    expect(true).to.be.true;
+  });
+});
