ab36c8d0262fa8dbb9a467fdc43d456fc6a39f4f
Satyajeet Parida <shatyajeet@gmail.com>
2017-08-20 21:04:38 +0530

    fix(build): Added webpack config
    webpack.config.babel.js has been added to generate the UMD builds

---
+++ webpack.config.babel.js
@@ -0,0 +1,20 @@
+import {join} from 'path';
+
+const include = join(__dirname, 'src');
+
+export default {
+  entry: './src/index.js',
+  output: {
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'wetherMap',
+  },
+  devtool: 'cheap-module-source-map',
+  module: {
+    rules: [{
+      test: /\.js/,
+      use: 'babel-loader',
+      include,
+    }],
+  },
+};
