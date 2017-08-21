6080598a29b16b53835ca425b0c153ad264c2d54
Satyajeet Parida <shatyajeet@gmail.com>
2017-08-21 18:59:53 +0530

    fix(weatherMap): Added Indian cities
    Added Delhi and Gurgaon to the iframe

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ export const weatherMap = (containerId) => {
   let iframeNode = document.createElement('iframe');
   iframeNode.src = `https://www.google.com/maps/embed/v1/directions
     ?key=AIzaSyAY0F9DQUGisVP1h_Q3ubkbvZfzyR4lGX0
-    &origin=Oslo+Norway
-    &destination=Telemark+Norway
+    &origin=Delhi+India
+    &destination=Gurgaon+India
     &avoid=tolls|highways`;
   iframeNode.height = 480;
   iframeNode.width = 640;
