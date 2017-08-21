05e7f879dfade0fe6eee4e492af8deed6d7e2485
Satyajeet Parida <shatyajeet@gmail.com>
2017-08-21 18:42:35 +0530

    feat(weatherMap): Added weatherMap demo functionality
    weatherMap now creates an iframe node and appends into the container element provided
    
    BREAKING: weatherMap console log has been removed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-export const weatherMap = () => {
-  console.log('This is the weather map');
+export const weatherMap = (containerId) => {
+  let containerElement = document.querySelector(containerId);
+  let iframeNode = document.createElement('iframe');
+  iframeNode.src = `https://www.google.com/maps/embed/v1/directions
+    ?key=AIzaSyAY0F9DQUGisVP1h_Q3ubkbvZfzyR4lGX0
+    &origin=Oslo+Norway
+    &destination=Telemark+Norway
+    &avoid=tolls|highways`;
+  iframeNode.height = 480;
+  iframeNode.width = 640;
+  containerElement.appendChild(iframeNode);
 };
